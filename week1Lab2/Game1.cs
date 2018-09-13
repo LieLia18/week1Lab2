966cbf3814f9d2a01db447d000c93917e6327a08
S00182102 <S00182102@LAB-D2039-14>
2018-09-13 11:48:58 +0100

    week1Lab1

--- week1Lab2/Game1.cs
+++ week1Lab2/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -11,7 +12,10 @@ namespace week1Lab2
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        Texture2D _txBoard;
+        Texture2D _txPac;
+        SoundEffect _backingTrack;
+        SpriteFont Font;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -39,7 +43,11 @@ namespace week1Lab2
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _txBoard = Content.Load<Texture2D>(@"Texture/gameboard");
+            _txPac = Content.Load<Texture2D>(@"Texture/pac");
+            _backingTrack = Content.Load<SoundEffect>(@"Audio/1b");
+            Font = Content.Load<SpriteFont>(@"Fonts/GameFont");
+            _backingTrack.Play();
             // TODO: use this.Content to load your game content here
         }
 
@@ -73,8 +81,14 @@ namespace week1Lab2
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            GraphicsDevice.Clear(Color.Green);
+            spriteBatch.Begin();
+            spriteBatch.Draw(_txBoard, new Rectangle(110, 110, _txBoard.Width/2, _txBoard.Height/2), Color.White);
+            //(_txPac, GraphicsDevice.Viewport.Bounds.Center.Vector2)
+            spriteBatch.Draw(_txPac, new Rectangle(200, 200, _txPac.Width/2, _txPac.Height/2), Color.White);
+            //(_txBoard, GraphicsDevice.Viewport.Bounds)
+            spriteBatch.DrawString(Font, "Hello Geogrie, I am Pennywise the Dancing Clown, This is a Nice Boat It will Float with you down here", new Vector2(100, 100), Color.PaleVioletRed);
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
